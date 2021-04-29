46fee94f4d8339d57d0b343d2fc7d80a65a74786
hassaanhashmi <hashmimu@msu.edu>
2021-04-29 12:33:55 -0400

    Register MAI env

--- gym_cstr_optim/__init__.py
+++ gym_cstr_optim/__init__.py
@@ -4,3 +4,7 @@ register(
     id='awgn-v0',
     entry_point='gym_cstr_optim.envs.res_alloc.awgn:AWGN',
 )
+register(
+    id='mai-v0',
+    entry_point='gym_cstr_optim.envs.res_alloc.mai:MAI',
+)
--- gym_cstr_optim/envs/__init__.py
+++ gym_cstr_optim/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_cstr_optim.envs.res_alloc.awgn import AWGN
+from gym_cstr_optim.envs.res_alloc.mai import MAI
